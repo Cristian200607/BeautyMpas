26aebdd985b458d6ae00d762e82fa2e4b4aab067
Cristian-01276 <pardorojascristianandres0@gmail.com>
2025-07-03 17:34:02 -0500

    implemetacion de filtro por catgeoria en js y modificaciones html

--- frontend/scripts/user/getProfesional.js
+++ frontend/scripts/user/getProfesional.js
@@ -1,35 +1,72 @@
 import {getProfesional} from '../../apis/apisProfesional.js'
+import {getProfesionalesPorCategoria} from '../../apis/apisCategoria.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
   
   const nombreElements = document.querySelectorAll('.texts p');
-  const botones = document.querySelectorAll('.btnVer');
+  const params = new URLSearchParams(window.location.search);
+  const idCategoria = params.get('id');
+  console.log('ID recibido desde la URL:', idCategoria);
 
   try{
-    
-    const data = await getProfesional()
-    console.log(data);
-    const listaProfesionales = data.profesionales;
-
-    listaProfesionales.forEach((pro, index) => {
-      if (index < nombreElements.length) {
-        nombreElements[index].textContent = pro.nombre;
+    if(idCategoria){
+      const data = await getProfesionalesPorCategoria(idCategoria);
+      console.log(data);
+      const listProfesionalByCategoria = data.profesionales;
+      const contenedor = document.querySelector('.container');
+      contenedor.innerHTML = ''; // Limpiar contenido anterior
+      if (listProfesionalByCategoria.length === 0) {
+        contenedor.innerHTML = `
+          <div class="sin-resultados">
+            <p>No hay profesionales disponibles en esta categoría por ahora.</p>
+          </div>
+        `;
+        return; // salir de la función para no seguir
       }
-      if (index < botones.length) {
-        botones[index].setAttribute('data-id', pro.id);
+      listProfesionalByCategoria.forEach(profesional => {
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.innerHTML = `
+          <div class="texts">
+            <p>${profesional.nombre}</p>
+            <button class="btnVer" data-id="${profesional.id}">RESERVA TU CITA YA</button>
+          </div>
+        `;
+
+        const boton = box.querySelector('.btnVer');
+        boton.addEventListener('click', () => {
+          window.location.href = `/frontend/pages/user/cuentaProfesional.html?id=${profesional.id}`;
+        });
+
+        contenedor.appendChild(box);
         
-      }
-    });
-
-    botones.forEach(btn => {
-      btn.addEventListener('click', function () {
-        const id = this.getAttribute('data-id');
-        console.log('ID al hacer clic:', id);
-        window.location.href = `/frontend/pages/user/cuentaProfesional.html?id=${id}`;
-
       });
-    });
 
+    }else{
+      const data = await getProfesional()
+      console.log(data);
+      const listaProfesionales = data.profesionales;
+      
+      const contenedor = document.querySelector('.container');
+      listaProfesionales.forEach(profesional => {
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.innerHTML = `
+          <div class="texts">
+            <p>${profesional.nombre}</p>
+            <button class="btnVer" data-id="${profesional.id}">RESERVA TU CITA YA</button>
+          </div>
+        `;
+
+        const boton = box.querySelector('.btnVer');
+        boton.addEventListener('click', () => {
+          window.location.href = `/frontend/pages/user/cuentaProfesional.html?id=${profesional.id}`;
+        });
+
+        contenedor.appendChild(box);
+        
+      });
+    }
   }catch(error) {
     console.error("Hubo un problema con la solicitud Fetch:", error);
     nombreElements.forEach(p => {
